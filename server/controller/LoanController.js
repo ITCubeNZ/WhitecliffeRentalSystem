1973e1ab9d26d8e7465726f53cea19f0d0b29003
Ben Olds <98551200+BenjaminCOlds@users.noreply.github.com>
2023-08-30 09:20:58 +1200

    Loan request completed

--- server/controller/LoanController.js
+++ server/controller/LoanController.js
@@ -1,5 +1,6 @@
-const { Int } = require("mssql");
 const con = require("../dbConfig");
+const nodemailer = require('nodemailer');
+const { emailPassword } = require("../securityDetails");
 
 module.exports.items_get = (req, res) => {
     // Used to get all items from the Database and return their information.
@@ -91,7 +92,7 @@ module.exports.loan_item_get = (req, res) => {
 
 module.exports.item_search = (req, res) => {
     // Used to return results based on item name. 
-    con.query(`SELECT * FROM item WHERE item_name LIKE "%${req.params.searchTerm}%"`, function (err, result, fields) {
+    con.query(`SELECT * FROM item WHERE item_name LIKE "%${req.body.item_code}%"`, function (err, result, fields) {
         if (err) {
             console.log(`Query Error Code: ${err.code}`)
             console.log(`Query Error Message: ${err.sqlMessage}`)
@@ -104,13 +105,19 @@ module.exports.item_search = (req, res) => {
 
 module.exports.item_search_code = (req, res) => {
     // Searches based on Serial Number
-    con.query(`SELECT * FROM item WHERE item_code = ${req.params.searchTerm}`, function (err, result, fields) {
+    con.query(`SELECT * FROM item WHERE item_code = '${req.body.item_code}'`, function (err, result, fields) {
         if (err) {
+            // Log errors and return a response back
             console.log(`Query Error Code: ${err.code}`)
             console.log(`Query Error Message: ${err.sqlMessage}`)
             res.status(406).json({ response: 'rejected' })
         } else {
-            res.status(201).json({ response: result })
+            // Check if result is empty and deliver and deliver a response based upon that.
+            if (result.length === 0) {
+                res.status(404).json({ response: 'No data was found' })
+            } else {
+                res.status(200).json({ result })
+            } 
         }
     })
 }
@@ -129,7 +136,7 @@ module.exports.item_search_location = (req, res) => {
 
 module.exports.item_search_description = (req, res) => {
     // Searches based on item description
-    con.query(`SELECT * FROM item WHERE item_description LIKE "%${req.params.searchTerm}%"`, function(err, result, feilds) {
+    con.query(`SELECT * FROM item WHERE item_description LIKE "%${req.params.searchTerm}%"`, function(err, result, fields) {
         if (err) {
             console.log(`Query Error Code: ${err.code}`)
             console.log(`Query Error Message: ${err.sqlMessage}`)
@@ -142,7 +149,6 @@ module.exports.item_search_description = (req, res) => {
 
 module.exports.loan_request = (req, res) => {
     // POST request for a student creating an original loan request. 
-    
     // Gets the current date to utilise within last updated. 
     const date = new Date()   
     let lastUpdated = `'${date.getFullYear()}-${String(date.getMonth()+1).padStart(2, "0")}-${date.getDate()}'`
@@ -159,13 +165,59 @@ module.exports.loan_request = (req, res) => {
             con.query(`INSERT INTO loan (student_id, item_id, loan_status, loan_date, return_date, last_updated) VALUES (${studentData[0].student_id}, ${req.body.item_id}, 'Pending', '${req.body.loan_date}', '${req.body.return_date}', ${lastUpdated})`, function (err, result, fields) {
                 if  (err) {
                     console.log(`Query Error Code: ${err.code}`)
-                    console.log(`Querry Error Message: ${err.sqlMessage}`)
+                    console.log(`Query Error Message: ${err.sqlMessage}`)
                     res.status(406).json({ response: 'rejected' })
                 } else {
-                    // Sends a response back to the user that loan request has been submitted has been submitted.
-                    res.status(201).json({ response: `Loan Request Submitted for ${studentData[0].f_name} ${studentData[0].l_name}.`})
+                    con.query(`SELECT * FROM item WHERE item_id = ${req.body.item_id}`, function (err, result, fields) {
+                        if (err) {
+                            console.log(`Query Error Code: ${err.code}`)
+                            console.log(`Query Error Message: ${err.sqlMessage}`)
+                            res.status(406).json({ response: 'rejected' })
+                        } else {
+                            const transporter = nodemailer.createTransport({
+                                service: 'Gmail',
+                                auth: {
+                                    user: 'whitecliffea@gmail.com',
+                                    pass: emailPassword
+                                }
+                            })
+                            
+                            const mailOption = {
+                                from: 'whitecliffea@gmail.com',
+                                to: studentData[0].email,
+                                subject: `Loan Request for ${studentData[0].f_name} ${studentData[0].l_name}`,
+                                text: `Thank you for submitting a request for ${result[0].item_name}. The item should be approved by staff soon and you'll receive a confirmation email when it is complete.`
+                            }
+        
+                            transporter.sendMail(mailOption, (error, info) => {
+                                if (error) {
+                                    console.log(error)
+                                    res.status(406).json({ response: 'Rejected' })
+                                } else {
+                                    // Sends a response back to the user that loan request has been submitted has been submitted.
+                                    res.status(201).json({ response: `Loan Request Submitted for ${studentData[0].f_name} ${studentData[0].l_name}.`})      
+                                }
+                            })
+                        }
+                    })
+                    
                 }
             })
+        }
+    })
+}
+
+module.exports.pending_requests_get = (req, res) => {
+    // Get all the pending requests
+    con.query(`SELECT * FROM loan WHERE loan_status = 'Pending'`, function (err, result, fields) {
+        if (err) {
+            // Log the errors to the console and deliver a response back 
+            console.log(`Query Error Code: ${err.code}`)
+            console.log(`Query Error Message: ${err.sqlMessage}`)
+            res.status(406).json({ response: 'rejected' })
+        } else {
+            // Deliver result back
+            res.status(200).json({ result })
         }
     })
 }
--- server/routes/LoanRoutes.js
+++ server/routes/LoanRoutes.js
@@ -8,5 +8,7 @@ router.get('/api/loans', loanController.loans_get)
 router.get('/api/loans/:id', loanController.loan_get)
 router.get('/api/loans/item/:id', loanController.loan_item_get)
 router.post('/api/loans/request', loanController.loan_request)
+router.get('/api/loans/pending', loanController.pending_requests_get)
+router.post('/api/items/search/item_code', loanController.item_search_code)
 
 module.exports = router
